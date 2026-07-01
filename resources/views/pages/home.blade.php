7a417fb6b23d5c97ad2f4642445bffaa0cc03995
monerapersian <monerapersian@gmail.com>
2026-07-01 13:58:20 +0330

    feat: setup blade layout and tailwind

---
+++ resources/views/pages/home.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.app')
+
+@section('title', 'خانه')
+
+@section('content')
+    <div class="min-h-screen flex items-center justify-center">
+        <h1 class="text-5xl font-bold text-blue-600">
+            Tailwind Works 🚀
+        </h1>
+    </div>
+@endsection
